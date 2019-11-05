ff194d89e5281822f61983b99019dc3829f94701
MXT <272590522@qq.com>
2019-11-05 18:01:00 +0800

    也是测试

---
+++ src/com/mxt/Test2.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: Test2
+ * Author:   27259
+ * Date:     2019/11/5 17:57
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.mxt;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 27259
+ * @create 2019/11/5
+ * @since 1.0.0
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("你知不知道！");
+    }
+}
